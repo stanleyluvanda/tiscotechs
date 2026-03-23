93471e8de3faf66f9bf6c1b730afd7df69d2d1e4
Stanley Luvanda <stanleyluvanda@gmail.com>
2026-03-23 15:37:24 -0400

    Commit Main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,15 @@
+
 // src/main.jsx
 import React from "react";
 import { createRoot } from "react-dom/client";
+import "./lib/amplify";                 // ✅ move this up first
 import App from "./App.jsx";
 import "./index.css";
-import BannerAdPortal from "./components/BannerAdPortal.jsx";  // ✅ add this line
-import "./lib/amplify";
+import BannerAdPortal from "./components/BannerAdPortal.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <BannerAdPortal />   {/* ✅ inject Google ad into #ad-banner */}
+    <BannerAdPortal />
   </React.StrictMode>
 );
