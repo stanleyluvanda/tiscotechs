8e20860923341fdd85d2aede21b69ed6ea062936
Stanley Luvanda <stanleyluvanda@gmail.com>
2026-05-25 13:00:16 -0400

    Commit reply composer

--- src/pages/UniversityAcademicPlatform.jsx
+++ src/pages/UniversityAcademicPlatform.jsx
@@ -2559,7 +2559,7 @@ function InlineComposer({ placeholder = "Write a comment…", onSubmit, isOpen, 
         <div className="mt-2 pl-0 sm:pl-8 flex items-start gap-2">
         <Avatar url={user?.photoUrl} name={user?.name} size="sm" online />
         {/*<div className="flex-1">*/}
-        <div className="flex-1 -ml-10 sm:ml-0">
+        <div className="flex-1 -ml-5 sm:ml-0">
           <InlineComposer
             placeholder="Reply…"
             onSubmit={(v, ra) => addReply(post.id, a.id, v, ra)}
