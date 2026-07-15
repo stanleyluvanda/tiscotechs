02495c0d07d9bb8e4ed9055a4477158f581141d6
Stanley Luvanda <stanleyluvanda@gmail.com>
2026-07-15 15:49:37 -0400

    Add lazy education data loader foundation

---
+++ src/data/eduConstants.js
@@ -0,0 +1,19 @@
+// src/data/eduConstants.js
+
+export const CONTINENTS = [
+  "Africa",
+  "Asia",
+  "Europe",
+  "LatinAmerica",
+  "NorthAmerica",
+  "Oceania",
+];
+
+export const YEARS = [
+  "Year 1",
+  "Year 2",
+  "Year 3",
+  "Year 4",
+  "Year 5",
+  "Graduate",
+];
